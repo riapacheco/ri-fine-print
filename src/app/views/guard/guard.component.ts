2ea459d13aa5be07ba2e83910cefa575d214389f
Ria Pacheco <me@riapacheco.com>
2022-10-11 00:41:43 -0600

    option to email link to self

--- src/app/views/guard/guard.component.ts
+++ src/app/views/guard/guard.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guard.component.scss']
 })
 export class GuardComponent implements OnInit {
+  formMode = false;
+  email = '';
+  errorMode = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSendEmail() {
+    if (!this.email) {
+      this.errorMode = true;
+    }
+    else if (!this.email.includes('@')) {
+      this.errorMode = true;
+    }
+    else {
+      this.errorMode = false;
+      const subject = 'A 🔗 for the FinePrint Web App';
+      const body = `Note to self: don't forget to visit https://fine-print.app`;
+      const sendOut = `mailto:${this.email}?subject=${subject}&body=${body}`;
+      window.open(sendOut);
+    }
+
+    
   }
 
 }
